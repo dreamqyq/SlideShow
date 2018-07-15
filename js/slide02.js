5c5742d17ab92d1428abda5d8aa24fbf4e3f35ec
enoch <qyqdream@gmail.com>
2018-07-15 17:27:32 +0800

    轮播2：解决部分bug

--- js/slide02.js
+++ js/slide02.js
@@ -5,7 +5,6 @@ var size = $slidePic.length
 var timer = null
 
 init(n)
-startSlide()
   
 $('.window').on('mouseover',function(){
   clearInterval(timer)
@@ -17,15 +16,15 @@ $('.window').on('mouseleave',function(){
 $dotsBtn.on('click',function(e){
   let index = $(e.currentTarget).index()
   console.log(index)
-  changePic(index-1)
+  changePic(judgeIndex(index-1))
   n = index
 })
 $('.nextBtn').on('click',function(event) {
-  changePic(n)
+  changePic(judgeIndex(n))
   n = n+1
 });
 $('.previousBtn').on('click',function(event) {
-  changePic(n-1)
+  changePic(judgeIndex(n-1))
   n = n-1
 })
 
@@ -36,28 +35,37 @@ function judgeIndex(index){
 function init(index){
   $slidePic.eq(index).addClass('current')
   $slidePic.eq(index).siblings().addClass('enter')
+  startSlide()
 }
 function startSlide(){
   timer = setInterval(() => {
     changePic(judgeIndex(n))
-    // console.log(judgeIndex(n))
     n++
-    },1500)
+    },2000)
   return timer
 }
 function changePic(index){
+  // 当前的图片：从current状态变为leave然后变为enter
   $slidePic.eq(index)
            .addClass('leave')
            .removeClass('current enter')
-           .one('transitionend',function(e){
-             $(e.currentTarget).addClass('enter')
-                               .removeClass('leave')
-             })
+           // .one('transitionend',function(e){
+           //   $(e.currentTarget).addClass('enter')
+           //                     .removeClass('leave')
+           //   })
+  // 后一张图片：从enter状态变为current状态
   $slidePic.eq(judgeIndex(index+1))
            .addClass('current')
            .removeClass('enter leave')
-           .siblings()
-           .removeClass('current')
+           // .siblings()
+           // .removeClass('current')
+  // 前一张图片：从enter状态变为leave状态
+ $slidePic.eq(judgeIndex(index-1))
+           .addClass('enter')
+           .removeClass('current leave')
+           // .siblings()
+           // .removeClass('current')
+            
   $dotsBtn.eq(judgeIndex(index+1)).addClass('active')
           .siblings().removeClass('active')
 }
