b8656e8d565ab3b82198673265bcac0a08236e46
enoch <qyqdream@gmail.com>
2018-07-19 23:45:10 +0800

    slide02:重写轮播逻辑

--- js/slide02.js
+++ js/slide02.js
@@ -4,68 +4,46 @@ var $dotsBtn = $('.dot')
 var size = $slidePic.length
 var timer = null
 
-init(n)
-  
-$('.window').on('mouseover',function(){
-  clearInterval(timer)
+$(document).on('visibilitychange',() => {
+  if(document.hidden){
+    window.clearInterval(timer)
+  }else{
+    timer = setInterval(() => {
+      changePic(n+1)
+    },2000)
+  }
 })
-$('.window').on('mouseleave',function(){
-  startSlide()
+timer = setInterval(() => {
+  changePic(n+1)
+},2000)
+
+$('.window').on('mouseover',() => {
+  window.clearInterval(timer)
+})
+$('.window').on('mouseleave',() => {
+  timer = setInterval(() => {
+    changePic(n+1)
+  },2000)
 })
 
 $dotsBtn.on('click',function(e){
   let index = $(e.currentTarget).index()
-  console.log(index)
-  changePic(judgeIndex(index-1))
-  n = index
+  changePic(index)
 })
-$('.nextBtn').on('click',function(event) {
-  changePic(judgeIndex(n))
-  n = n+1
-});
-$('.previousBtn').on('click',function(event) {
-  changePic(judgeIndex(n-1))
-  n = n-1
+$('.nextBtn').on('click',function(){
+  changePic(n+1)
+  n ++
+})
+$('.previousBtn').on('click',function(){
+  changePic(n-1)
+  n --
 })
 
-function judgeIndex(index){
-  index = index % size
-  return index
-}
-function init(index){
-  $slidePic.eq(index).addClass('current')
-  $slidePic.eq(index).siblings().addClass('enter')
-  startSlide()
-}
-function startSlide(){
-  timer = setInterval(() => {
-    changePic(judgeIndex(n))
-    n++
-    },2000)
-  return timer
-}
 function changePic(index){
-  // 当前的图片：从current状态变为leave然后变为enter
-  $slidePic.eq(index)
-           .addClass('leave')
-           .removeClass('current enter')
-           // .one('transitionend',function(e){
-           //   $(e.currentTarget).addClass('enter')
-           //                     .removeClass('leave')
-           //   })
-  // 后一张图片：从enter状态变为current状态
-  $slidePic.eq(judgeIndex(index+1))
-           .addClass('current')
-           .removeClass('enter leave')
-           // .siblings()
-           // .removeClass('current')
-  // 前一张图片：从enter状态变为leave状态
- $slidePic.eq(judgeIndex(index-1))
-           .addClass('enter')
-           .removeClass('current leave')
-           // .siblings()
-           // .removeClass('current')
-            
-  $dotsBtn.eq(judgeIndex(index+1)).addClass('active')
-          .siblings().removeClass('active')
+  index = index % size
+  console.log(`current-index:${index}`)
+  $slidePic.css('transform',`translateX(${-1280*index}px)`)
+  $dotsBtn.eq(index).addClass('active').siblings().removeClass('active')
+  n ++
+  console.log(`than-index:${n%size}`)
 }
